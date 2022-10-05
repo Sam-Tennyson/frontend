d9d1f1ba3e34f1cecbffb2a0d9e2fcde04b261cc
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2022-10-05 20:17:15 +0530

    fdfdfdf

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,8 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4685384744669195"
+     crossorigin="anonymous"></Script>
         {/* <Script data-ad-client="" async src="" /> */}
         <Script strategy={"lazyOnload"} async src={`https://www.googletagmanager.com/gtag/js?id${process.env.GA_ID}`} />
         <Script
